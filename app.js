0af15de8609373dead5d526b105cfa4b4a3d9fd2
Vi <gavialus@hotmail.com>
2020-03-06 18:14:37 +0000

    inicializacion de la aplicaicon

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 //requireds nativo de NODEJS
 //const fs = require('fs');
 
-const multiplicar= require ('./multiplicar/multiplicar');
-let base = 5
+const {crearArchivo}= require ('./multiplicar/multiplicar');
 
-console.log(multiplicar)
-// crearArchivo(base)
-//             .then (archivo => console.log(`Archivo creado: ${archivo}`))
-//             .catch(e=>console.log(e))
+let argv = process.argv;
+let parametro = argv[2];
+let base = parametro.split('=')[1]
+
+//console.log(base)
+
+
+crearArchivo(base)
+            .then (archivo => console.log(`Archivo creado: ${archivo}`))
+            .catch(e=>console.log(e))
 
 // let data = ''
 
